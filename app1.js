0de2f33d50e0e3b15356c589d68968bed27dc8b4
wasi <mw7262@gmail.com>
2020-07-14 16:01:30 +0500

    till chapter 43

---
+++ app1.js
@@ -0,0 +1,133 @@
+        // chapter 38-42 Q1
+
+        // function power(){
+        //     var a= +prompt("Enter base");
+        //     var b= +prompt("Enter power");  
+        //     console.log(Math.pow(a,b))
+        // }
+        // power();
+
+        // chapter 38-42 Q2
+
+        // function leap(){
+        //     var year= prompt("Enter year");
+        //     if(year%4===0){
+        //         document.write(year+" is a Leap year")
+        //     }
+        //     else{
+        //         document.write(year+" is not a Leap year") 
+        //     }
+        // }
+        // leap();
+
+        // chapter 38-42 Q3
+
+        // var a= +prompt("Enter sides of triangle");
+        // var b= +prompt("Enter sides of triangle");
+        // var c= +prompt("Enter sides of triangle");
+        // function TriArea(){
+        //     var x= (a+b+c)/2;
+        //     area = x*(x-a)*(x-b)*(x-c);
+        //     document.write("Area of triangle having sides "+a+", "+b+" and "+c+" is "+ area)
+        // }
+    
+        // function area(){
+        //     return TriArea();;
+        // }
+        // area();
+
+        // chapter 38-42 Q4
+
+        // var Eng= +prompt("Enter english marks");
+        // var Math= +prompt("Enter Math marks");
+        // var Bio= +prompt("Enter Bio marks");
+        // var total= 300;
+
+        // function average(){
+        //    var a= (Eng+Math+Bio)/3
+        //    document.write("Average marks: "+ a + "<br>");
+        // }
+        // function percent(){
+        //     var per = (Eng+Math+Bio)*100/total;
+        //     document.write("Percentage: "+ per)
+        // }
+        // function main(){
+        //     return  average() + percent();
+        // }
+        // main();
+
+          // chapter 38-42 Q6
+ 
+//  var a= "a quick brown fox jumps over the lazy dog"
+//  var b= a.split("");
+// for(i=0 ; i<b.length ; i++){
+//      if(b[i]=== "a"||b[i]=== "e" || b[i]=== "i"
+//      ||b[i]=== "o" ||b[i]=== "u"){
+//         var c= b.splice(i,1)
+//      }
+//  }
+//  var final= b.toString()
+//  document.write(final )
+
+
+
+  // chapter 38-42 Q8
+ 
+//  var dist = +prompt("Enter distance in KM of two points");
+ 
+// function meter(){
+//     var mtr = dist * 1000;
+//     document.write("Distance in meters is "+ mtr +"<br>")
+// }
+
+// function feet(){
+//     var feet = dist * 1000 * 3.3;
+//     document.write("Distance in feets is "+ feet  +"<br>")
+// }
+// function inches(){
+//     var inch = dist * 1000* 3.3*12;
+//     document.write("Distance in inches is "+ inch  +"<br>")
+// }
+// function cm(){
+//     var cm = dist * 1000*100;
+//     document.write("Distance in Centi meters is "+ cm +"<br>")
+// }
+
+
+// meter ()
+// feet()
+// inches ()
+// cm()
+
+
+//   chapter 38-42 Q9
+
+// function otPay(){
+// var rate= 12;
+// var time = +prompt ("Enter hours ");
+// var pay = rate * time;
+// document.write(pay);
+// }
+// otPay ()
+
+
+//   chapter 38-42 Q10
+
+// function CalcAmount(){
+// var amount= +prompt ("Enter amount to withdraw");
+// var hund= amount % 100
+// var hundred = (amount - hund)/100;
+// var fif = hund %50;
+// var fifty = (hund - fif )/50
+// var ten = (fif- fif%10)/10
+
+// document.write("you will have "+hundred+" hundred notes "+ fifty+" fifty notes "+ten+" tens notes");
+// }
+// CalcAmount ()
+
+
+
+
+
+
+
